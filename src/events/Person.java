6b8354c96fd7ade19bba7571d92e90054cf20714
panxchina <panxcina1@foxmail.com>
2016-03-25 17:37:31 +0800

    20160325

---
+++ src/events/Person.java
@@ -0,0 +1,14 @@
+package events;
+
+public class Person {
+
+    private Long id;
+    private int age;
+    private String firstname;
+    private String lastname;
+
+    public Person() {}
+
+    // Accessor methods for all properties, private setter for 'id'
+
+}
